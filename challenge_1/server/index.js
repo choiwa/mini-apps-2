c7b322ddb041d133f7ea51d76cd3f6c2a00824e6
Joyce Cho <joyjoycho@gmail.com>
2018-08-30 18:57:05 -0700

    get lowdb to receive data from server.json

--- challenge_1/server/index.js
+++ challenge_1/server/index.js
@@ -4,12 +4,15 @@ const router = jsonServer.router('data/db.json');
 const middlewares = jsonServer.defaults();
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync('db.json')
+const adapter = new FileSync('data/db.json')
 const db = low(adapter)
 
 server.use(middlewares);
 
+console.log(db.get('events').find({date: "-300"}).value());
+
 server.get('/', (req, res) => {
+
   // console.log({body: res.locals.data});
   // res.jsonp({
   //   body: res.locals.data
