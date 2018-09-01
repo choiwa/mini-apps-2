8c03579a16ad54c3493f0ae0bca34ab4614dd17c
Joyce Cho <joyjoycho@gmail.com>
2018-09-01 14:41:20 -0700

    clean up

--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -7,8 +7,4 @@ app.use(express.static(path.join(__dirname, '../public')))
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello express');
-// })
-
 app.listen(3000, () => console.log("listening to port 3000"));
