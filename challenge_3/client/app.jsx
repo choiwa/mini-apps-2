2ca08cb2d1586a23c264ccbf4e78deb95d272c1e
Joyce Cho <joyjoycho@gmail.com>
2018-09-03 02:47:03 -0700

    create numbers of pins

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import Square from './square.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
   render() {
     return (
-      <div>hello React</div>
+      <div>
+        <div>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+        <div>
+          {this.renderSquare(9)}
+        </div>
+
+      </div>
     )
   }
 }
